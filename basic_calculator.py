9555765c515b31befa3650c82314e561118b6d84
P Manoj K Reddy <manojkreddy007@gmail.com>
2025-04-04 19:50:35 +0530

    Add files via upload

---
+++ basic_calculator.py
@@ -0,0 +1,14 @@
+#Assignment 1
+#task 1/2
+num1 = input("Enter the first Number: ")
+num2 = input("Enter the Second Number: ")
+num1 = int(num1)
+num2 = int(num2)
+addition = (num1+num2)
+subtraction = (num1-num2)
+multiplication = (num1*num2)
+division = (num1/num2)
+print("Addition: ",addition)
+print("Subtraction: ",subtraction)
+print("Multiplication: ",multiplication)
+print("Division: ",division)
