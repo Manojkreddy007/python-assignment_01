46ca3bb8741663fe4768458e91d22610dd75c6be
P Manoj K Reddy <manojkreddy007@gmail.com>
2025-04-04 19:51:34 +0530

    Add files via upload

---
+++ name_greetings.py
@@ -0,0 +1,6 @@
+#Assignment 1
+#Task 2/2
+name1 = input("Enter your First name: ").title()
+name2 = input("Enter your Last name: ").title()
+fullname = name1 + ' ' + name2 + '!'
+print ("Hello,",fullname,"Welcome to the Python program.")
